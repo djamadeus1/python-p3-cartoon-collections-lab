6fabb1ab74e03eb1e3de14f44312619b1fcffd38
Ronald Roberts <djamadeus1@gmail.com>
2024-11-20 12:30:18 -0500

    must make shorter

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,27 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(lst):
+    num = 0
+    for every_dwarf in lst:
+        num += 1
+        print(str(num) + ". " + every_dwarf)
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(lst):
+    result = []
+    for every_word in lst:
+        new_word = every_word.capitalize() + "!"
+        result.append(new_word)
+    return result    
 
-def long_planeteer_calls():
-    pass
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(lst):
+    for every_word in lst:
+        if len(every_word) > 4:
+            return True
+    return False
+
+def find_the_cheese(lst):
+    cheese = ["cheddar", "gouda", "camembert"]
+    result = [word for word in cheese if word in lst]
+    if len(result) > 0:
+        return result[0]
+    else:
+        return None
